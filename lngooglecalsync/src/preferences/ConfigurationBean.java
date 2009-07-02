bb78be500dc7cf31449bc711583c62b14ce6b680
sterneck <sterneck@01c50daf-2647-4964-ae55-24bea4a17eaa>
2009-07-02 11:55:46 +0000

    work on feature to save settings to configuration file for later loading
    git-svn-id: svn://svn.code.sf.net/p/lngooglecalsync/code@33 01c50daf-2647-4964-ae55-24bea4a17eaa

--- lngooglecalsync/src/preferences/ConfigurationBean.java
+++ lngooglecalsync/src/preferences/ConfigurationBean.java
@@ -1,8 +1,28 @@
 package preferences;
 
+import java.util.Properties;
+
 public class ConfigurationBean {
 
+    public static void main(String[] a) {
+        ConfigurationBean cb = new ConfigurationBean();
+        cb.writeConfig();
+    }
+
+
     public void writeConfig() {
+
+        Properties config = new Properties();
+        config.setProperty("LotusNotesMailURL", getLotusMailFileURL());
+        config.setProperty("GoogleUsername", getGoogleUserName());
+        config.setProperty("GooglePassword", getGooglePassword());
+        config.setProperty("GoogleEnableProxy", new Boolean(getGoogleEnableProxy()).toString());
+        config.setProperty("GoogleProxyIP", getGoogleProxyIP());
+        config.setProperty("GoogleProxyPort", getGoogleProxyPort());
+        config.setProperty("GoogleUploadToMainCalendar", new Boolean(getGoogleUploadToMainCalendar()).toString());
+        config.setProperty("GoogleUseSSL", new Boolean(getGoogleUseSSL()).toString());
+
+
     }
 
     public void readConfig() {
@@ -11,52 +31,66 @@ public class ConfigurationBean {
     public void setLotusMailFileURL(String lotusMailFileURL) {
         this.lotusMailFileURL = lotusMailFileURL;
     }
+
     public String getLotusMailFileURL() {
         return lotusMailFileURL;
     }
+
     public void setGoogleUserName(String googleUserName) {
         this.googleUserName = googleUserName;
     }
+
     public String getGoogleUserName() {
         return googleUserName;
     }
+
     public void setGooglePassword(String googlePassword) {
         this.googlePassword = googlePassword;
     }
+
     public String getGooglePassword() {
         return googlePassword;
     }
+
     public void setGoogleEnableProxy(boolean googleEnableProxy) {
         this.googleEnableProxy = googleEnableProxy;
     }
+
     public boolean getGoogleEnableProxy() {
         return googleEnableProxy;
     }
+
     public void setGoogleProxyIP(String googleProxyIP) {
         this.googleProxyIP = googleProxyIP;
     }
+
     public String getGoogleProxyIP() {
         return googleProxyIP;
     }
+
     public void setGoogleProxyPort(String googleProxyPort) {
         this.googleProxyPort = googleProxyPort;
     }
+
     public String getGoogleProxyPort() {
         return googleProxyPort;
     }
+
     public void setGoogleUploadToMainCalendar(boolean googleUploadToMainCalendar) {
         this.googleUploadToMainCalendar = googleUploadToMainCalendar;
     }
+
     public boolean getGoogleUploadToMainCalendar() {
         return googleUploadToMainCalendar;
     }
+
     public void setGoogleUseSSL(boolean googleUseSSL) {
         this.googleUseSSL = googleUseSSL;
     }
+
     public boolean getGoogleUseSSL() {
         return googleUseSSL;
     }
-
     String lotusMailFileURL;
     String googleUserName;
     String googlePassword;
